9b27a175b6430d888a6a020468a94ff290e13387
Kenny Black and joshua rinard <kenny+joshua@>
2017-02-22 14:14:33 -0800

    add create food

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,7 +1,7 @@
 class FoodsController < ApplicationController
 
   def index
-    @foods = Food.all
+    @foods = Food.where(user_id: current_user.id)
   end
 
   def new
@@ -10,14 +10,14 @@ class FoodsController < ApplicationController
 
   def create
     @food = Food.new(food_params)
-    if @food.save
-      flash[:notice] = "Food Successfuly Added!"
-      respond_to do |format|
-        format.html {redirect_to foods_path}
+    respond_to do |format|
+      if @food.save
+        format.html {redirect_to foods_path, notice: 'Food Successfuly Added!'}
         format.js
+      else
+        format.html {render :new, notice: 'There was an error.'}
+        format.js { redirect_back(fallback_location: foods_path, notice: 'There was an error.') }
       end
-    else
-      render :new
     end
   end
 
@@ -26,6 +26,6 @@ class FoodsController < ApplicationController
 
   private
     def food_params
-      params.require(:food).permit(:name, :amount, :calories, :protein, :fat, :carbs, :notes, :date)
+      params.require(:food).permit(:name, :amount, :calories, :protein, :fat, :carbs, :notes, :date, :user_id)
     end
 end

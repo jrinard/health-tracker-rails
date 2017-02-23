757eda420ab21afbe27c4a261594e21631c148e2
Kenny Black and joshua rinard <kenny+joshua@>
2017-02-22 16:47:38 -0800

    refactored testing

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -1,3 +1,5 @@
-RSpec.configure do |config|
-  config.include FactoryGirl::Syntax::Methods
-end
+# May not need
+
+# RSpec.configure do |config|
+#   config.include FactoryGirl::Syntax::Methods
+# end
